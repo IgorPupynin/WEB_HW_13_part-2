fd532c42c535a1e895420d4ae291ae4cbce2fe43
Игорь Пупынин (Igor Pupinin) <igorpupynin18@gmail.com>
2023-06-04 13:24:25 +0300

    Add files via upload

---
+++ hw_project/users/urls.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.views import PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView
+from django.urls import path
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    path('signup/', views.signup_user, name='signup'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('password_reset/', views.ResetPasswordView.as_view(), name='password_reset'),
+    path('password_reset/done/', PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset/confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html',
+                                          success_url='/users/password_reset/complete/'),
+         name='password_reset_confirm'),
+    path('password_reset/complete/',
+         PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
+]
